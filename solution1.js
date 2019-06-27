ca3d2adc9b82b55fe3a155f785593aa9b281a916
Christopher Tso <christso@gmail.com>
2019-06-27 10:57:39 +1000

    Fix logic to pass test cases

--- solution1.js
+++ solution1.js
@@ -1,4 +1,18 @@
-var solution = function (a,b) {
-  return a + b;
+var solution = function (A) {
+  A.sort((a,b) => a > b);
+
+  let pArr = new Array();
+  A.forEach((value, index) => {
+    var pValue = pArr.length > 0 ? pArr[pArr.length - 1] : null;
+    if (value > 0 && pValue && value - pValue > 1) {
+      return;
+    }
+
+    pArr.push(value);
+  });
+
+  var lastValue = pArr[pArr.length - 1];
+  return Math.max(1, lastValue + 1);
 };
+
 module.exports = solution;
--- test/test1.js
+++ test/test1.js
@@ -1,10 +1,30 @@
 var assert = require('assert');
 var solution = require('../solution1');
 
-describe('Solution 1', function() {
-  describe('solution', function() {
-    it('should return solution when condition', function() {
-      assert.equal(solution(4, 3), 7);
+describe('Solution 1 - smallest positive N not in A', function() {
+  describe('Simple 1', function() {
+    it('should return smallest N not in A', function() {
+      assert.equal(solution([1, 3, 6, 4, 1, 2]), 5);
+    });
+  });
+  describe('Simple 2', function() {
+    it('should return smallest N not in A', function() {
+      assert.equal(solution([1, 2, 3]), 4);
+    });
+  });
+  describe('Simple 3', function() {
+    it('should return smallest N not in A ', function() {
+      assert.equal(solution([-1, -3]), 1);
+    });
+  });
+  describe('Single', function() {
+    it('should return smallest N not in A', function() {
+      assert.equal(solution([1]), 2);
+    });
+  });
+  describe('Negative Positive', function() {
+    it('should return smallest N not in A', function() {
+      assert.equal(solution([-1000, 100000, 50000]), 1);
     });
   });
 });
